c533c3527828f5032ee67c3e25dbaedcd57436ce
Trần Anh Tuấn <tuanvip870@gmail.com>
2022-10-21 09:58:09 +0700

    Update Counting Patterns.cpp

--- Counting Patterns.cpp
+++ Counting Patterns.cpp
@@ -1,5 +1,4 @@
 #include <bits/stdc++.h>
-#include <unordered_map>
 using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
@@ -9,94 +8,154 @@ typedef pair <int,int> ii;
 #define st first
 #define nd second
 #define pb push_back
-#define lb lower_bound
-#define ub upper_bound
-#define er equal_range
-#define bin binary_search
 #define vll vector<ll>
 #define vi vector<int>
 #define all(v) (v).begin(), (v).end()
-#define FOR(i,x,y) for(ll i = x; i <= y; i++)
-#define FOS(i,x,y) for(ll i = x; i >= y; i--)
-const ll SIZE = 1e5 + 5;
+#define FOR(i,x,y) for(ll i = x; i <= y; ++i)
+#define FOS(i,x,y) for(ll i = x; i >= y; --i)
+#define EACH(i,x) for (auto &(i) : (x))
+#define el cout << '\n';
 const ll MOD = 1e9 + 7;
+
 #define dbg(...) cerr << "[" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "]  "
 #define dbge(...) cerr << "[" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "]" << endl;
+
+/*
 mt19937_64 rd(chrono::steady_clock::now().time_since_epoch().count());
 ll rand(ll l, ll r) { return uniform_int_distribution<ll>(l, r)(rd); }
+*/
 
-map<string, int> ans;
-struct TrieNode{
-    struct TrieNode *child[26] = {NULL};;
-    int End = 0;
-    int count = 0;
+//#define int long long
+const ll N = 2e5 + 5;
+
+struct node
+{
+    map<int, node*> tree;
+    node* link;
+    int cnt;
+
+    node()
+    {
+        link = nullptr;
+        cnt = 0;
+    }
 };
 
-struct TrieNode *NewNode(void)
+node* newNode()
 {
-    struct TrieNode *pNode = new TrieNode;
- 
-    pNode->End = false;
- 
-    for (int i = 0; i < 26; i++)
-        pNode->child[i] = NULL;
- 
-    return pNode;
+    node* n = new node;
+    return n;
 }
 
-void add(TrieNode *root, string s, bool&check){
-    TrieNode *Insert = root;
-    for(int i = 0; i < s.size(); i++){
-        int pos = s[i] - 'a';
-        if(!Insert -> child[pos]){
-            Insert -> child[pos] = new TrieNode();
-            Insert -> count++;
-        }
-        if(Insert -> count > 1) check = 0;
-        Insert = Insert -> child[pos];
+void insert(node* root, string s)
+{
+    node* v = root;
+    for (int x : s)
+    {
+        if (!v -> tree[x])
+            v -> tree[x] = newNode();
+        v = v -> tree[x];
     }
-    Insert -> End = 1;
 }
 
-void xuli(TrieNode *root, string t, int j){
-    string s;
-    TrieNode *kt = root, *save = NULL;
-    for(int i = j; i < t.size(); i++){
-        s += t[i];
-        int pos = t[i] - 'a';
-        if(!kt -> child[pos]) return;
-        kt = kt -> child[pos];
-        if(kt -> End) ans[s]++;
+void build(node* root)
+{
+    queue<node*> que;
+    que.push(root);
+
+    while (!que.empty())
+    {
+        node* v = que.front();
+        que.pop();
+
+        for (int i = 'a'; i <= 'z'; i++)
+            if (v->tree.count(i) > 0)
+            {
+                que.push(v -> tree[i]);
+                v -> tree[i] -> link = root;
+                node * p = v -> link;
+                while (p)
+                {
+                    if (p->tree.count(i) > 0)
+                    {
+                        v -> tree[i] -> link = p -> tree[i];
+                        break;
+                    }
+
+                    if (!p -> link)
+                        break;
+                    p = p -> link;
+                }
+            }
+    }
+}
+
+vector<pair<node*, int> > vv;
+
+void dfs(node *u, int h)
+{
+    vv.push_back({u, h});
+
+    for (int i = 'a'; i <= 'z'; ++i)
+    {
+        if (u->tree.count(i) > 0)
+        {
+            dfs(u->tree[i], h + 1);
+        }
     }
 }
 
 signed main()
 {
-    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    //freopen(file".INP","r",stdin);
-    //freopen(file".OUT","w",stdout);
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    str s; cin >> s;
+    int n; cin >> n;
 
-    int n; string s; bool check = 1;
-    cin >> s >> n;
-    vector<string> a(n); TrieNode *root = new TrieNode();
-    for(int i = 0; i < n; i++){
-        cin >> a[i];
-        if(s.find(a[i][a[i].size() - 1]) != string::npos 
-		&& s.size() >= a[i].size())
-            add(root, a[i], check);
+    node* root = newNode();
+
+    vector<string> t(n);
+    for (auto &x : t)
+    {
+        cin >> x;
+        insert(root, x);
     }
-    if(check){
-        int d = s.size();
-        for(int i = 0; i < n; i++){
-            cout << d << endl;
-            d--;
+
+    build(root);
+
+    node* v = root;
+    for (int x : s)
+    {
+        while (1)
+        {
+            if (v->tree.count(x) > 0)
+            {
+                v = v -> tree[x];
+                break;
+            }
+
+            if (!v -> link)
+                break;
+            v = v -> link;
         }
-        return 0;
+        v -> cnt++;
     }
-	for(int i = 0; i < s.size(); i++) xuli(root, s, i);
-    for(int i = 0; i < n; i++)
-        cout << ans[a[i]] << endl;
-    
+
+    dfs(root, 1);
+
+    sort(vv.begin(), vv.end(), [&](pair<node*, int> L, pair<node*, int> R) {
+        return L.nd > R.nd;
+    });
+
+    for (auto current : vv)
+        if (current.st -> link)
+            current.st -> link -> cnt += current.st -> cnt;
+
+    for (auto x : t)
+    {
+        node* v = root;
+        for (int c : x)
+            v = v -> tree[c];
+        cout << v -> cnt << "\n";
+    }
     return 0;
 }
-
