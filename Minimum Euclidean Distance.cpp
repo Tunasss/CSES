9127745401a5dc9f40d0a0c66818fef05be39fa6
Trần Anh Tuấn <tuanvip870@gmail.com>
2022-10-22 07:45:54 +0700

    Update Minimum Euclidean Distance.cpp

--- Minimum Euclidean Distance.cpp
+++ Minimum Euclidean Distance.cpp
@@ -1,65 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
+//A fish was here.
 typedef long long ll;
-typedef unsigned long long ull;
-typedef string str;
-typedef pair <int, int> ii;
-#define file "TEST"
-#define st first
-#define nd second
-#define pb push_back
-#define lb lower_bound
-#define ub upper_bound
-#define vll vector<ll>
-#define vi vector<int>
-#define all(v) (v).begin(), (v).end()
-#define FOR(i,x,y) for(ll i = x; i <= y; ++i)
-#define FOS(i,x,y) for(ll i = x; i >= y; --i)
-#define EACH(i,x) for (auto &(i) : (x))
-#define el cout << '\n';
-const ll MOD = 1e9 + 7;
-#define dbg(...) cerr << "[" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "]  "
-#define dbge(...) cerr << "[" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "]" << endl;
+#define int long double
+typedef pair <int, int> P;
+#define X first
+#define Y second
 
-/*
-mt19937_64 rd(chrono::steady_clock::now().time_since_epoch().count());
-ll rand(ll l, ll r) { return uniform_int_distribution<ll>(l, r)(rd); }
-*/
-
-#define int long long
-const ll N = 2e5 + 5;
-
-struct point {
-	int x, y;
-} a[N];
-
-int s = 50;
-
-bool cmp(const point &a, const point &b) {
-	return a.x * a.y < b.x * b.y;
-}
-
-int calc(point x, point y) {
-	return (x.x - y.x) * (x.x - y.x) + (x.y - y.y) * (x.y - y.y);
+int norm(P a, P b) {
+    return (b.X - a.X) * (b.X - a.X) + (b.Y - a.Y) * (b.Y - a.Y);
 }
 
 signed main() {
-	ios_base::sync_with_stdio(0); cin.tie(0);
-	//freopen(file".INP","r",stdin);
-	//freopen(file".OUT","w",stdout);
+    int n; cin >> n;
+    vector<P> v(n);
+    int d = INT_MAX;
+    for (int i = 0; i < n; i++) {
+        int x, y; cin >> x >> y;
+        v[i] = {x, y};
+    }
+    sort(v.begin(), v.end());
+    set<P> s = {{v[0].Y, v[0].X}};
+    int j = 0;
+    for (int i = 1; i < n; i++) {
+        auto it = s.begin();
+        int dd = ceil(sqrt(d));
+        while (j < i && v[j].X < v[i].X - dd) {
+            s.erase({v[j].Y, v[j].X});
+            j++;
+        }
 
-	int ans = (int)1e20;
-	int n; cin >> n;
-
-	FOR(i, 1, n)  cin >> a[i].x >> a[i].y;
-
-	sort(a + 1, a + n + 1, cmp);
-
-	FOR(i, 1, n) {
-		for (int j = i + 1; j <= n && j <= i + s; j++) {
-			ans = min(ans, calc(a[i], a[j]));
-		}
-	}
-
-	cout << ans; el;
-}
+        auto l = s.lower_bound({v[i].Y - dd, 0});
+        auto r = s.upper_bound({v[i].Y + dd, 0});
+        for (auto it = l; it != r; it++) {
+            d = min(d, norm({it->Y, it->X}, v[i]));
+        }
+        s.insert({v[i].Y, v[i].X});
+    }
+    cout << fixed << setprecision(6) << d;
+}   
